0c4a42bba90322177bcb9aaaa4c8f3c2d77b52a4
Jiabao Li <56912131+TIMESTICKING@users.noreply.github.com>
2024-03-22 17:38:41 +0800

    add altrasonic and using freertos

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 
 #include <Wire.h>
 #include <Adafruit_AMG88xx.h>
+#include <HCSR04.h>
+
+void IR_reader(void *para);
+void ultrasonic_reader(void * para);
+void rtos_setup();
+void IR_setup();
+void send_float(u_char * num);
+
 
 Adafruit_AMG88xx amg;
 
@@ -17,8 +25,6 @@ Adafruit_AMG88xx amg;
 
 volatile bool intReceived = false;
 uint8_t pixelInts[8];
-char headers[2] = {0x77, 0x88};
-char tail = 0x66;
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 /******* 
 we can tell which pixels triggered the interrupt by reading the
@@ -36,6 +42,14 @@ byte 7 |  0      0      0      0      0      0      0      0
 
 *****/
 
+TaskHandle_t *IR_read_handler;
+TaskHandle_t *sonic_read_handler;
+
+// ultrasonic settings
+int trigger_pin = 32;
+int echo_pin = 33;
+HCSR04 ultrasonicSensor(trigger_pin, echo_pin, 15, 200);
+
 void AMG88xx_ISR() {
   //keep your ISR short!
   //we don't really want to be reading from or writing to the sensor from inside here.
@@ -47,9 +61,19 @@ void setup() {
   pinMode(INT_PIN, INPUT);
   
   Serial.begin(115200);
-  Serial.println("AMG88xx interrupt test");
 
+  IR_setup();
+  ultrasonicSensor.begin();
+
+  rtos_setup();
+
+}
+
+
+void IR_setup() {
   bool status;
+
+  Serial.println("AMG88xx interrupt test");
   
   // default settings
   status = amg.begin();
@@ -68,52 +92,84 @@ void setup() {
 
   //attach to our Interrupt Service Routine (ISR)
   attachInterrupt(digitalPinToInterrupt(INT_PIN), AMG88xx_ISR, FALLING);
-
 }
 
-// void loop() {
-  
-//   if(intReceived){
-//     //get which pixels triggered
-//     amg.getInterrupt(pixelInts);
+void rtos_setup(){
+  xTaskCreate(
+    IR_reader
+    , "IRread"
+    , 3000
+    , NULL
+    , 1
+    , IR_read_handler
+  );
 
-//     Serial.println("**** interrupt received! ****");
-//     for(int i=0; i<8; i++){
-//       Serial.println(pixelInts[i], BIN);
-//     }
-//     Serial.println();
+  xTaskCreate(
+    ultrasonic_reader
+    , "SonicRead"
+    , 4000
+    , NULL
+    , 1
+    , sonic_read_handler
+  );
+}
 
-//     //clear the interrupt so we can get the next one!
-//     amg.clearInterrupt();
-    
-//     intReceived = false;
-//   }
-// }
 
-void loop() { 
+
+void IR_reader(void *para) { 
+  static char headers[2] = {0x77, 0x88};
+  static char tail = 0x66;
+  while (1)
+  {  
     //read all the pixels
     amg.readPixels(pixels);
 
-    // Serial.print("[");
-    // for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
-    //   Serial.print(pixels[i-1]);
-    //   Serial.print(", ");
-    //   if( i%8 == 0 ) Serial.println();
-    // }
-    // Serial.println("]");
-    // Serial.println();
-
     Serial.print(headers);
     for(int i=0; i<AMG88xx_PIXEL_ARRAY_SIZE; i++){
-      unsigned char *byteArray = (unsigned char *)&pixels[i];
-    
-      // 发送4个字节
-      for (int j = 0; j < sizeof(float); j++) {
-        Serial.write(byteArray[j]);
-      }
+      send_float((u_char *)&pixels[i]);
     }
     Serial.print(tail);
 
     //delay a second
-    delay(100);
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+
+void ultrasonic_reader(void * para){
+  static char headers[2] = {0xAF, 0xFA};
+  static char tail = 0xFF;
+  float distance = 0;
+  float filterd_distance = 0;
+  while (1)
+  {
+    distance = ultrasonicSensor.getDistance();
+
+    
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+
+    filterd_distance = ultrasonicSensor.getMedianFilterDistance(); //pass 3 measurements through median filter, better result on moving obstacles
+
+
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+  
+}
+
+
+void send_float(u_char * num){
+  // 发送4个字节
+  for (int j = 0; j < sizeof(float); j++) {
+    Serial.write(num[j]);
+  }
+}
+
+
+void loop(){
+    // UBaseType_t uxHighWaterMark = uxTaskGetStackHighWaterMark( sonic_read_handler );
+    // Serial.print("SampleTask Stack High Water Mark:");
+    // Serial.println(uxHighWaterMark);
 }
